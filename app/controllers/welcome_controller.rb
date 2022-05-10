28b549dde7241ca29ce2afc66381dc078d37df3f
Dora Horel <dora.horel@smartpension.co.uk>
2022-05-10 12:46:21 +0100

    Fix merging issues

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
+  DEFAULT_LIMIT = 12
+
   def index
-    DEFAULT_LIMIT = 12
-    
     @shows = Show.take((params[:limit] || DEFAULT_LIMIT).to_i)
   end
 end
